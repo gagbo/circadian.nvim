aa9eae7a365d595cb2853dc093115b1b1490d3d1
Kaleb Smart <kalebsma@gmail.com>
2025-03-22 09:03:13 -0400

    Split set_theme into two functions
    Split set_theme into two functions:
    
    One handles the timer, one handles actually setting the theme

--- lua/circadian/init.lua
+++ lua/circadian/init.lua
@@ -6,61 +6,48 @@ local M = {}
 
 local lustrous = require("circadian.lustrous")
 
-local timer
-
 local function get_timezone_offset_hours()
   local now = os.time()
   return (os.difftime(now, os.time(os.date("!*t", now))) / 3600.0)
 end
 
-local function set_theme(opts)
-  local day = opts.day or { background = "light", colorscheme = nil }
-  local night = opts.night or { background = "dark", colorscheme = nil }
+local timer
+
+local function set_theme(name, theme)
+  local need_reset = false
+  if theme.background then
+    vim.o.background = theme.background
+    need_reset = true
+  end
+  if theme.colorscheme then
+    vim.cmd("colorscheme " .. theme.colorscheme)
+    need_reset = true
+  end
+  if need_reset then
+    vim.cmd("syntax reset")
+  end
+  vim.notify(
+    "Circadian: " .. name .. " detected (" .. theme.colorscheme .. " / " .. theme.background .. ")",
+    vim.log.levels.INFO,
+    { title = "Circadian" }
+  )
+end
+
+local function main(opts)
+  local default = {
+    day = { background = "light", colorscheme = nil },
+    night = { background = "dark", colorscheme = nil },
+  }
   local lat = opts.lat or 48.8567879
   local lon = opts.lon or 2.3510768
 
   local current, rise, set = lustrous.get_time({ lat = lat, lon = lon, offset = get_timezone_offset_hours() })
-
   local next_time = rise
-
+  set_theme(current, opts[current] or default[current])
   if current == "day" then
     next_time = set
-    local need_reset = false
-    if day.background then
-      vim.o.background = day.background
-      need_reset = true
-    end
-    if day.colorscheme then
-      vim.cmd("colorscheme " .. day.colorscheme)
-      need_reset = true
-    end
-    vim.notify(
-      "Circadian: day detected (" .. day.colorscheme .. " / " .. day.background .. ")",
-      vim.log.levels.INFO,
-      { title = "Circadian" }
-    )
-    if need_reset then
-      vim.cmd("syntax reset")
-    end
   else
     next_time = rise
-    local need_reset = false
-    if night.background then
-      vim.o.background = night.background
-      need_reset = true
-    end
-    if night.colorscheme then
-      vim.cmd("colorscheme " .. night.colorscheme)
-      need_reset = true
-    end
-    vim.notify(
-      "Circadian: night detected (" .. night.colorscheme .. " / " .. night.background .. ")",
-      vim.log.levels.INFO,
-      { title = "Circadian" }
-    )
-    if need_reset then
-      vim.cmd("syntax reset")
-    end
   end
 
   local delay = next_time - os.time()
@@ -78,12 +65,12 @@ local function set_theme(opts)
   )
 
   return vim.defer_fn(function()
-    set_theme(opts)
+    main(opts)
   end, delay * 1000)
 end
 
 function M.setup(opts)
-  timer = set_theme(opts)
+  timer = main(opts)
 end
 
 function M.stop()
