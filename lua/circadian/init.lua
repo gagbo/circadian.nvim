7c983de041254751296c87ede6f11000169cea2e
Kaleb Smart <kalebsma@gmail.com>
2025-03-22 08:54:26 -0400

    Prevent theme switching with no delay
    I was seeing an issue right along the boundary of sunrise/sunset where
    it would constantly notify that the theme had been changed with an
    extremely short delay and would continue like this until I had restart
    nvim. While investigating this, I also noticed that the timers are in
    ms, but this was being calculated in seconds.
    
    I converted the vim.loop timer to vim.defer_fn. vim.loop as been
    deprecated in favor of vim.uv anyways, and vim.defer_fn is more
    aligned with what is trying to be done here (one-off timers being
    created at varying frequency).

--- lua/circadian/init.lua
+++ lua/circadian/init.lua
@@ -35,11 +35,12 @@ local function set_theme(opts)
       need_reset = true
     end
     vim.notify(
-      "Circadian: day detected (" .. day.colorscheme .. " / " .. day.background ..  ")",
+      "Circadian: day detected (" .. day.colorscheme .. " / " .. day.background .. ")",
       vim.log.levels.INFO,
-    {title = "Circadian"})
+      { title = "Circadian" }
+    )
     if need_reset then
-        vim.cmd("syntax reset")
+      vim.cmd("syntax reset")
     end
   else
     next_time = rise
@@ -53,34 +54,32 @@ local function set_theme(opts)
       need_reset = true
     end
     vim.notify(
-      "Circadian: night detected (" .. night.colorscheme .. " / " .. night.background ..  ")",
+      "Circadian: night detected (" .. night.colorscheme .. " / " .. night.background .. ")",
       vim.log.levels.INFO,
-    {title = "Circadian"})
-    if need_reset then vim.cmd("syntax reset") end
+      { title = "Circadian" }
+    )
+    if need_reset then
+      vim.cmd("syntax reset")
+    end
   end
 
-  local timer = vim.loop.new_timer()
   local delay = next_time - os.time()
   -- Detecting when next_time is in the past
   -- Looping an extra day like that is likely to be 2/3 minutes off though
   if delay < 0 then
     delay = delay + 86400
+  elseif delay == 0 then
+    delay = 1
   end
   vim.notify(
-    "Circadian: next change planned at " .. os.date('%H:%M:%S', next_time) .. " (in " .. delay .. " seconds)",
+    "Circadian: next change planned at " .. os.date("%H:%M:%S", next_time) .. " (in " .. delay .. " seconds)",
     vim.log.levels.DEBUG,
-  {title = "Circadian"})
+    { title = "Circadian" }
+  )
 
-  timer:start(delay,
-              0,
-              function ()
-                timer:stop()
-                timer:close()
-                vim.schedule_wrap(function()
-                  set_theme(opts)
-                end)
-              end)
-  return timer
+  return vim.defer_fn(function()
+    set_theme(opts)
+  end, delay * 1000)
 end
 
 function M.setup(opts)
@@ -91,11 +90,9 @@ function M.stop()
   if timer then
     timer:stop()
     timer:close()
-    vim.notify("Call circadian.setup again with the arguments to restart", vim.log.levels.INFO,
-    {title = "Circadian"})
+    vim.notify("Call circadian.setup again with the arguments to restart", vim.log.levels.INFO, { title = "Circadian" })
   else
-    vim.notify("Circadian is not running now", vim.log.levels.WARN,
-    {title = "Circadian"})
+    vim.notify("Circadian is not running now", vim.log.levels.WARN, { title = "Circadian" })
   end
 end
 
